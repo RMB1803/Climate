ea46cacb1bf39d67054f31338d88271e3faca8b4
Ram <rammohanofficial03@gmail.com>
2025-05-16 02:22:28 +0530

    Some UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import './App.css'
-import { Button } from './components/ui/button'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import Layout from './components/component/Layout'
+import { ThemeProvider } from './context/theme-provider'
+import WeatherDashboard from './pages/WeatherDashboard.tsx'
+import CityPage from './pages/CityPage.tsx'
 
 function App() {
 
+  const queryClient = new QueryClient()
+
   return (
-    <BrowserRouter>
-      <Layout>Hello</Layout>
-      {/* <Routes>
-        <Route />
-      </Routes> */}
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme='dark'>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<WeatherDashboard />} />
+              <Route path='/city/:cityName' element={<CityPage />}/>
+            </Routes> 
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
